424c681af7e366e7b874df9f715a3b4d057881cb
NJ3rsey <oleg.volpres@gmail.com>
2022-04-06 12:11:16 +0300

    ping, sql

---
+++ ipv2.py
@@ -0,0 +1,109 @@
+import ping3
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import settings
+
+ping3.EXCEPTIONS = True
+TOKEN = settings.API
+# получаем экземпляр `Updater`
+updater = Updater(token=TOKEN, use_context=True)
+# получаем экземпляр `Dispatcher`
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+# функция обратного вызова
+def echo(update, context):
+    # добавим в начало полученного сообщения строку 'ECHO: '
+    text = 'ECHO: ' + update.message.text
+    # `update.effective_chat.id` - определяем `id` чата,
+    # откуда прилетело сообщение
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=text)
+
+
+def check(update, context):
+    try:
+        ping3.verbose_ping(context.args[0], count=6)
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Все ок.")
+    except ping3.errors.TimeToLiveExpired as err:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=err.ip_header["src_addr"]) # TimeToLiveExpired, DestinationUnreachable and DestinationHostUnreachable have ip_header and icmp_header attached.
+    except ping3.errors.HostUnknown:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Неизвестный или неверный хост.")
+    except ping3.errors.Timeout:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Время запроса истекло, хост недоступен или не отвечает."
+                                      "\n(Request timeout for ICMP packet)")
+    except IndexError:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Хост не указан")
+
+
+def caps(update, context):
+    # если аргументы присутствуют
+    if context.args:
+        # объединяем список в строку и
+        # переводим ее в верхний регистр
+        text_caps = ' '.join(context.args).upper()
+        # `update.effective_chat.id` - определяем `id` чата,
+        # откуда прилетело сообщение
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                text=text_caps)
+    else:
+        # если в команде не указан аргумент
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                text='No command argument')
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                text='send: /caps argument')
+
+
+# Обратите внимание, что из обработчика в функцию
+# передаются экземпляры `update` и `context`
+def start(update, context):
+    # `bot.send_message` это метод Telegram API
+    # `update.effective_chat.id` - определяем `id` чата,
+    # откуда прилетело сообщение
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="I'm a bot, please talk to me!")
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Sorry, I didn't understand that command.")
+
+
+# говорим обработчику, если увидишь команду `/start`,
+# то вызови функцию `start()`
+start_handler = CommandHandler('start', start)
+# добавляем этот обработчик в `dispatcher`
+dispatcher.add_handler(start_handler)
+
+# говорим обработчику `MessageHandler`, если увидишь текстовое
+# сообщение (фильтр `Filters.text`)  и это будет не команда
+# (фильтр ~Filters.command), то вызови функцию `echo()`
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+# регистрируем обработчик `echo_handler` в экземпляре `dispatcher`
+dispatcher.add_handler(echo_handler)
+
+# обработчик команды '/caps'
+caps_handler = CommandHandler('caps', caps)
+# регистрируем обработчик в диспетчере
+dispatcher.add_handler(caps_handler)
+
+# обработчик команды /ip
+check_handler = CommandHandler('check', check)
+# регистрируем обработчик в диспетчере
+dispatcher.add_handler(check_handler)
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+# запуск прослушивания сообщений
+updater.start_polling()
+# обработчик нажатия Ctrl+C
+updater.idle()
---
+++ settings.py
@@ -0,0 +1,28 @@
+import pymysql
+
+TO = 15891589
+FROM = 5124580170
+bot = '-1158vgdpmOId6m7NPek'
+end = 'AAHITOAGRNMnABC'
+
+api = (TO+FROM).__str__() + ":" + end + bot
+API = api.__str__()
+
+
+def sql(ppk):
+    connection = pymysql.connect(host='localhost',
+                                 database='TEST',
+                                 user='bot',
+                                 password='password')
+    try:
+        q = """ SELECT * FROM PPK WHERE num = """ '\'' + ppk.__str__() + '\'' """ """ # Query request of PPK needed
+        cursor = connection.cursor()
+        cursor.execute(q)
+        connection.commit()
+        rec = cursor.fetchall()
+        for row in rec:
+            print("number: ", row[0])
+        cursor.close()
+    finally:
+        connection.close()
+        print("MySQL connection is closed")
